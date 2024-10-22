2ebe87b48efcac7faee9d6e12c1ee1b94768d792
Ashley Lago <ashl3ylag0@gmail.com>
2024-10-22 18:25:31 -0400

    Started server

--- api/actions/actions-model.js
+++ api/actions/actions-model.js
@@ -9,6 +9,8 @@ module.exports = {
   remove,
 };
 
+// Resolves to an array of all the resources contained in the database. If you pass an id to this 
+//  method it will return the resource with that id if one is found.
 function get(id) {
   let query = db('actions');
 
@@ -30,12 +32,17 @@ function get(id) {
   }
 }
 
+// Calling insert passing it a resource object will add it to the database and return the newly 
+//  created resource.
 function insert(action) {
   return db('actions')
     .insert(action)
     .then(([id]) => get(id));
 }
 
+// Accepts two arguments, the first is the id of the resource to update, and the second is an 
+//  object with the changes to apply. It returns the updated resource. If a resource with the 
+//  provided id is not found, the method returns null.
 function update(id, changes) {
   return db('actions')
     .where('id', id)
@@ -43,6 +50,8 @@ function update(id, changes) {
     .then((count) => (count > 0 ? get(id) : null));
 }
 
+// The remove method accepts an id as its first parameter and, upon successfully deleting the 
+//  resource from the database, returns the number of records deleted.
 function remove(id) {
   return db('actions').where('id', id).del();
 }
--- api/projects/projects-model.js
+++ api/projects/projects-model.js
@@ -10,6 +10,8 @@ module.exports = {
   getProjectActions,
 };
 
+// Resolves to an array of all the resources contained in the database. If you pass an id to this 
+//  method it will return the resource with that id if one is found.
 function get(id) {
   let query = db("projects as p");
 
@@ -36,12 +38,17 @@ function get(id) {
   }
 }
 
+// Calling insert passing it a resource object will add it to the database and return the newly 
+//  created resource.
 function insert(project) {
   return db("projects")
     .insert(project)
     .then(([id]) => get(id));
 }
 
+// Accepts two arguments, the first is the id of the resource to update, and the second is an 
+//  object with the changes to apply. It returns the updated resource. If a resource with the 
+//  provided id is not found, the method returns null.
 function update(id, changes) {
   return db("projects")
     .where("id", id)
@@ -49,12 +56,15 @@ function update(id, changes) {
     .then(count => (count > 0 ? get(id) : null));
 }
 
+// The remove method accepts an id as its first parameter and, upon successfully deleting the 
+//  resource from the database, returns the number of records deleted.
 function remove(id) {
   return db("projects")
     .where("id", id)
     .del();
 }
 
+// Takes a project id as its only argument and returns a list of all the actions for the project.
 function getProjectActions(projectId) {
   return db("actions")
     .where("project_id", projectId)
